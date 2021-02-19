d0b0b5b896012c3fc2a4eb7ae100815b7e97bf6e
vica1083mail <79304776+vica1083mail@users.noreply.github.com>
2021-02-19 13:21:48 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# as
+# all
 Created with CodeSandbox
---
+++ src/quiz/level3.jsx
@@ -0,0 +1,63 @@
+import React, { useRef } from "react";
+
+export const QuizLevel3 = ({
+  // Here is the list of functions that you should use
+  sendName,
+  sendPhone,
+  sendCookingDone,
+  sendProgrammingDone,
+  sendWashingDone,
+  sendStylingDone
+}) => {
+  // Create other refs here
+  const nameRef = useRef();
+  const phone = useRef();
+  const cookingRef = useRef();
+  const washedacar = useRef();
+  const styledaroom = useRef();
+  const progrannedaform = useRef();
+
+  // Add some code inside this function for sending data from the form to the document
+  const applyFormData = (e) => {
+    e.preventDefault();
+    sendName(nameRef.current.value);
+    sendPhone("+375" + phone.current.value);
+    sendCookingDone(cookingRef.current.checked);
+    sendWashingDone(washedacar.current.checked);
+    sendStylingDone(styledaroom.current.checked);
+    sendProgrammingDone(progrannedaform.current.checked);
+  };
+
+  return (
+    <form className="document-input" onSubmit={(e) => applyFormData(e)}>
+      <label htmlFor="name-input">Name:</label>
+      {/* Hang refs on all other inputs like this */}
+      <input id="name-input" placeholder="Teddy Brown" ref={nameRef} />
+
+      <label htmlFor="phone-input">Phone:</label>
+      <input id="phone-input" placeholder="44 123 45 67" ref={phone} />
+
+      <label htmlFor="activities-input">Done:</label>
+
+      <div id="activities-input">
+        <input type="checkbox" id="cooking" ref={cookingRef} />
+        <label htmlFor="cooking">Cooked 3 burgers</label>
+        <br />
+
+        <input type="checkbox" id="washing" ref={washedacar} />
+        <label htmlFor="washing">Washed a car</label>
+        <br />
+
+        <input type="checkbox" id="styling" ref={styledaroom} />
+        <label htmlFor="styling">Styled a room</label>
+        <br />
+
+        <input type="checkbox" id="programming" ref={progrannedaform} />
+        <label htmlFor="programming">Programmed a form</label>
+        <br />
+      </div>
+
+      <input type="submit" value="Apply" id="apply-button" />
+    </form>
+  );
+};
